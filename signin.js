0b8f976e6258bb06430ad8f27200c23bfa3716d3
Charger1423jr <156366687+Charger1423jr@users.noreply.github.com>
2024-03-20 01:10:27 -0400

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,1 +1,36 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+let email_check = 0;
+
+function getEmail() {
+    readline.question("Email (Case Sensitive): ", function(email) {
+        if (email === emailTest) {
+            email_check = 1;
+            getPassword();
+        } else {
+            console.log("Invalid email. Please try again.");
+            getEmail();
+        }
+    });
+}
+
+function getPassword() {
+    let passwrd_check = 0;
+    readline.question("Password (Case Sensitive): ", function(password) {
+        if (password === passTest) {
+            passwrd_check = 1;
+        }
+        if (email_check === 1 && passwrd_check === 1) {
+            console.log("Confirmed Sign In");
+            readline.close();
+        } else {
+            console.log("Invalid password. Please try again.");
+            getPassword();
+        }
+    });
+}
+
+getEmail();
